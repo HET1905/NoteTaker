e5e13f4ed541b2a731026a3ac9db88304d4e6fa3
Hetal Patel <nphetal1905@gmail.com>
2019-08-06 20:16:31 -0400

    put code in js file

--- routing/html-routes.js
+++ routing/html-routes.js
@@ -3,23 +3,6 @@ var mysql = require('mysql');
 var router = require('express').Router();
 var connection = require('./connection.js');
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "password",
-//     database: "notetaker_db"
-// });
-
-// // Initiate MySQL Connection.
-// connection.connect(function (err) {
-//     if (err) {
-//         console.error(`error connecting: ${ err.stack}`);
-//         return;
-//     }
-//     console.log(`connected as id  ${connection.threadId}`);
-// });
-
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname + '../public/index.html'));
 });
@@ -39,12 +22,10 @@ router.get('/api/notes', (req, res) => {
 });
 
 router.post('/api/notes',(req,res)=>{
-    // console.log(req.body);
     connection.query(`INSERT INTO notes(title,body) values(?,?)`,[req.body.title,req.body.body],(err,result)=>{
         if(err){
             console.log(`Error in inserting data in table ${err}`);
         }
-            // res.send(true);
             res.redirect('/');  
         
         
@@ -55,7 +36,6 @@ router.post('/api/notes',(req,res)=>{
 router.delete('/api/notes/:id', function(req, res) {
     connection.query("DELETE FROM notes WHERE id = ?", [req.params.id], function(err, result) {
       if (err) {
-        // If an error occurred, send a generic server failure
         return res.status(500).end();
       }
      
